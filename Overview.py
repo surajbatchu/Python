02af03a1606d96dace12063aee22864b2829b770
surajbatchu <batchusuraj12@gmail.com>
2025-01-09 11:49:58 +0530

    first commit

---
+++ Overview.py
@@ -0,0 +1,3 @@
+#Python is dynamically typed Programming language.
+#In Python while declaring variable, we not need to Mention Data Types.
+#Data types will be assigned by language based on value stored into the variable.
---
+++ Variables.py
@@ -0,0 +1,16 @@
+a = 10
+b = 20
+print('Addition of two numbers is:', a+b)
+print(type(a))
+c = 3+4j
+print(c, type(c))
+name = "sai suraj"
+print(name[0:3])
+print(name[-5:])
+print(len(name))
+print(name.upper())
+print(name.lower())
+print(name.find("suraj"))
+print(name.split(","))
+print(name.replace("sai","surya"))
+text = ['apple','bananna','pinapple','orange']
